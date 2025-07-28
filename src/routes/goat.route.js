94fb3aff6c1c56f32b1b6e58fff53d74e232aa8e
Ramayuda Mahardika <ramayudaid@gmail.com>
2025-07-28 19:32:34 +0700

    add api-doc

--- src/routes/goat.route.js
+++ src/routes/goat.route.js
@@ -2,14 +2,15 @@ import express from 'express';
 import * as goatController from '../controllers/goat.controller.js';
 import { validate } from '../middleware/validate.middleware.js';
 import { createGoatSchema, updateGoatSchema } from '../validators/goat.validator.js';
+import { authenticate } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/', validate(createGoatSchema), goatController.createGoat);
+router.post('/', authenticate, validate(createGoatSchema), goatController.createGoat);
 router.get('/', goatController.getAllGoats);
 router.get('/:id', goatController.getGoatDetail);
-router.put('/:id', validate(updateGoatSchema), goatController.updateGoat);
-router.delete('/:id', goatController.deleteGoat);
-router.get('/:id/qrcode', goatController.generateGoatQRCode);
+router.put('/:id', authenticate, validate(updateGoatSchema), goatController.updateGoat);
+router.delete('/:id', authenticate, goatController.deleteGoat);
+router.get('/:id/qrcode', authenticate, goatController.generateGoatQRCode);
 
 export default router;
